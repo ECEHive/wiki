f08ec5eed4274335f64c6b391a7121dfd393f1c1
amwnag <amwang36@gmail.com>
2024-09-30 17:45:40 -0400

    minor: sidebar ordering

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,15 +34,15 @@ export default defineConfig({
 					],
 				},
 				{
+					label: 'Workshop Docs',
+					autogenerate: { directory: 'workshop' },
+				},
+				{
 					label: 'Policies',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Rules and Policies', slug: 'policies/policies-main' },
 					],
-				},
-				{
-					label: 'Workshop Docs',
-					autogenerate: { directory: 'workshop' },
 				},
 			],
 		}),
