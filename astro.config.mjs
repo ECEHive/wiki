8def83db7708fbd17d5894826a20a678f5fa0ab0
amwnag <amwang36@gmail.com>
2025-04-14 18:07:39 -0400

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,81 +1,84 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ecehive.github.io/',
-    base: '/wiki',
-    integrations: [starlight({
-        title: 'The Hive Wiki',
-        editLink: {
-            baseUrl: 'https://github.com/ecehive/wiki/edit/main/',
+  site: "https://ecehive.github.io/",
+  base: "/wiki",
+  integrations: [
+    starlight({
+      title: "The Hive Wiki",
+      editLink: {
+        baseUrl: "https://github.com/ecehive/wiki/edit/main/",
+      },
+      customCss: ["./src/styles/global.css", "./src/styles/tailwind.css"],
+      favicon: "/favicon.svg",
+      logo: {
+        light: "./public/wiki-logo-light.svg",
+        dark: "./public/wiki-logo-dark.svg",
+        replacesTitle: true,
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/ecehive/wiki",
         },
-        customCss: [
-            './src/styles/global.css',
-			'./src/styles/tailwind.css',
-        ],
-        favicon: '/favicon.svg',
-        logo: {
-            light: './public/wiki-logo-light.svg',
-            dark: './public/wiki-logo-dark.svg',
-            replacesTitle: true
+      ],
+      sidebar: [
+        {
+          label: "Explore",
+          items: [
+            "explore/intro",
+            "explore/electronics-benchtop",
+            "explore/3d-printing",
+            "explore/laser-cutting",
+            "explore/embedded-systems",
+            "explore/pcb",
+            "explore/machine-shop",
+            "explore/crafts",
+          ],
         },
-        social: {
-            github: 'https://github.com/ecehive/wiki',
+        {
+          label: "Workshop Docs",
+          autogenerate: { directory: "workshop" },
         },
-        sidebar: [
-            {
-                label: 'Explore',
-                items: [
-                    'explore/intro',
-                    'explore/electronics-benchtop',
-                    'explore/3d-printing',
-                    'explore/laser-cutting',
-                    'explore/embedded-systems',
-                    'explore/pcb',
-                    'explore/machine-shop',
-                    'explore/crafts',
-                ],
-            },
-            {
-                label: 'Workshop Docs',
-                autogenerate: { directory: 'workshop' },
-            },
-        ],
-		}), 
-		tailwind({
-			applyBaseStyles: false,
-		})],
-    markdown: {
-        rehypePlugins: [
-            rehypeSlug,
-            [
-                rehypeAutolinkHeadings, 
-                { 
-                    behavior: 'append',
-                    headingProperties: {
-                        className: ['anchor'],
-                    },
-                    properties: {
-                        className: ['anchor-link'],
-                    }
-                }
-            ],
-            [
-                rehypeExternalLinks,
-                {
-                    properties: {
-                        className: ['external'],
-                    },
-                    target: '_blank',
-                },
-            ],
-        ],
-    },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          headingProperties: {
+            className: ["anchor"],
+          },
+          properties: {
+            className: ["anchor-link"],
+          },
+        },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          properties: {
+            className: ["external"],
+          },
+          target: "_blank",
+        },
+      ],
+    ],
+  },
 });
